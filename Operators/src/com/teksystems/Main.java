5ba198a3497c466c60d5eaeea4656322c559b828
TheMarqker <61724267+TheMarqker@users.noreply.github.com>
2022-03-17 14:19:23 -0400

    Add files via upload

---
+++ Operators/src/com/teksystems/Main.java
@@ -0,0 +1,160 @@
+package com.teksystems;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = 8;
+
+        int sum = x++ + y;
+
+        System.out.println(sum);
+
+        int z = x + 1;
+
+        System.out.println(z);
+    }
+}
+
+
+
+
+/*
+    Write the following integers in binary notation. Do not use any Java functions or online conversion applications to
+     calculate the answer as this will hinder the learning process and your understanding of the concept. However, you
+      may check your answers using Java methods.
+
+1       = 1
+8       = 1000
+33      = 100001
+78      = 1001110
+787     = 1100010011
+33,987  = 1000010011000011
+
+    Convert the following binary numbers to decimal notation. Do not use any Java functions or online conversion
+    applications to calculate the answer as this will hinder the learning process and your understanding of the concept.
+     However, you may check your answers using Java methods.
+
+0010                    = 10
+1001                    = 1001
+0011 0100               = 11
+0111 0010               = 111
+0010 0001 1111          = 10
+0010 1100 0110 0111     = 10
+
+
+
+    Write a program that declares an integer a variable x and assigns the value 2 to it and prints out the binary string
+     version of the number ( Integer.toBinaryString(x) ). Now, use the left shift operator (<<) to shift by 1 and assign
+     the result to x. Before printing the results, write a comment with the predicted decimal value and binary string.
+     Now, print out x in decimal form and in binary notation.
+
+values:9 17 88
+
+        int x = 2;
+        String binary = Integer.toBinaryString(x);
+
+        System.out.println(binary); // 0010
+        System.out.println(x); // 2
+
+        x = x << 1;
+        binary = Integer.toBinaryString(x);
+
+        System.out.println(binary); // 0100
+        System.out.println(x); // 4
+
+        x = 9; // 1001 //input for 17 and 88
+        x = x << 1;
+        binary = Integer.toBinaryString(x);
+
+        System.out.println(binary); // 0001 0010
+        System.out.println(x); // 18
+
+
+    Write a program that declares a variable x and assigns 150 to it and prints out the binary string version of the
+    number. Now use the right shift operator (>>) to shift by 2 and assign the result to x. Write a comment indicating
+     what you anticipate the decimal and binary values to be. Now print the value of x and the binary string.
+
+Do the preceding exercise with the following values: 225 1555 32456
+
+        int x = 150;
+        String Binary = Integer.toBinaryString(x);
+
+        System.out.println(Binary); // 1001 0110
+        System.out.println(x); // 150
+
+        x = x >> 2;
+        Binary = Integer.toBinaryString(x);
+
+        System.out.println(Binary); // 0010 0101
+        System.out.println(x); // 37
+
+        x = 225; // 1110 0001
+        x = x >> 2;
+        Binary = Integer.toBinaryString(x);
+
+        System.out.println(Binary); // 0011 1000
+        System.out.println(x); // 56
+
+    Write a program that declares 3 int variables x, y, and z. Assign 7 to x and 17 to y. Write a comment that
+    indicates what you predict will be the result of the bitwise & operation on x and y. Now use the bitwise &
+    operator to derive the decimal and binary values and assign the result to z.
+
+Now, with the preceding values, use the bitwise | operator to calculate the “or” value between x and y. As before,
+write a comment that indicates what you predict the values to be before printing them out.
+
+        int x = 7; // 0000 0111
+        int y = 17; // 0001 0001
+        int z;
+
+        z = 7 & 17; // true
+
+        System.out.println(z);
+
+        z = 7 | 17; // 23
+
+        System.out.println(z);
+
+
+    Write a program that declares an integer variable, assigns a number, and uses a postfix increment operator to
+    increase the value. Print the value before and after the increment operator.
+        int x = 3;
+
+		System.out.println(x);
+
+		x++;
+
+		System.out.println(x);
+
+
+    Write a program that demonstrates at least 3 ways to increment a variable by 1 and does this multiple times.
+    Assign a value to an integer variable, print it, increment by 1, print it again, increment by 1, and then print again.
+        int x = 3;
+		System.out.println(x);
+
+		x++;
+		System.out.println(x);
+
+		++x;
+		System.out.println(x);
+
+		x += 1;
+		System.out.println(x);
+
+    Write a program that declares 2 integer variables, x, and y, and then assigns 5 to x and 8 to y. Create another
+     variable sum and assign the value of ++x added to y and print the result. Notice the value of the sum
+     (should be 14). Now change the increment operator to postfix (x++) and re-run the program.
+     Notice what the value of sum is. The first configuration incremented x and then calculated the sum while the
+     second configuration calculated the sum and then incremented x.
+        int x = 5;
+		int y = 8;
+
+		int sum = x++ + y;
+
+		System.out.println(sum);
+
+		int z = x + 1;
+
+		System.out.println(z);
+ */
